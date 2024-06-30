664ae5c4d310b99d97604d3f4f0343539aae6551
pedroraffo97 <59334649+pedroraffo97@users.noreply.github.com>
2024-06-30 18:56:19 +0200

    ContentView.swift.version3

---
+++ ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  BelloApp
+//
+//  Created by Pedro Raffo Leon on 20.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @EnvironmentObject var data:Routine
+    @EnvironmentObject var  routinetracker: RoutineTracker
+    @State var isLoading = true
+    var body: some View {
+        Group{
+            if isLoading {
+                VStack {
+                    RegistrationView()
+                }
+            }
+            else {
+                MenuView()
+            }
+        }
+        .onAppear {
+            //Simulate a background task or data loading
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                withAnimation{
+                    self.isLoading = false
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
