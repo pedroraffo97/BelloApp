8f4eb4be5e87c1eead7dac8deaa28bfa0d45dbd3
pedroraffo97 <59334649+pedroraffo97@users.noreply.github.com>
2024-06-30 19:02:27 +0200

    ExerciseGlossaryView.swift.version7

---
+++ ExerciseGlossaryView.swift
@@ -0,0 +1,132 @@
+//
+//  ExerciseGlossaryView.swift
+//  BelloApp
+//
+//  Created by Pedro Raffo Leon on 04.11.23.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct ExerciseGlossaryView: View {
+    @EnvironmentObject var exerciseglossary: ExerciseGlossary
+    @EnvironmentObject var data: Routine
+    @State private var searchText: String = ""
+    @State var selectRoutine: String = ""
+    
+    var filteredExercises: [Exercise] {
+        if searchText.isEmpty {
+            return exerciseglossary.ExerciseGuide
+        }
+        else {
+            return exerciseglossary.ExerciseGuide.filter { exercise in
+                exercise.name.localizedCaseInsensitiveContains(searchText.lowercased()) ||
+                exercise.bodypart.localizedCaseInsensitiveContains(searchText.lowercased()) ||
+                exercise.display.localizedCaseInsensitiveContains(searchText.lowercased())
+            }
+        }
+    }
+    
+    var body: some View {
+            VStack{
+                NavigationView{
+                    List{
+                        ForEach(filteredExercises, id: \.self){exercise in
+                            NavigationLink{
+                                ScrollView{
+                                    LazyVStack {
+                                        Text("Information")
+                                            .font(.title)
+                                        Image(exercise.display)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width:   50, height: 50, alignment: .center)
+                                            .padding()
+                                        Text(exercise.name)
+                                            .padding(0.05)
+                                            .font(.headline)
+                                        Text(exercise.bodypart)
+                                            .font(.callout)
+                                            .padding()
+                                        HStack{
+                                            Image(exercise.displayInstructions)
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width:   200, height: 200, alignment: .center)
+                                                .padding()
+                                        }
+                                        .padding()
+                                        Text(exercise.information)
+                                            .padding()
+                                        Text(exercise.execution)
+                                            .padding()
+                                        YoutubeVideoView(videoID: exercise.videolink)
+                                            .frame(width: 300, height: 200, alignment: .center)
+                                            .padding()
+                                    }
+                                    .navigationTitle(exercise.name)
+                                    .toolbar {
+                                        ToolbarItem{
+                                            NavigationLink("Add"){
+                                                Button{
+                                                    data.new_routine
+                                                        .append(exercise)
+                                                } label:{
+                                                    Text("new Lifting Routine")
+                                                }
+                                                .buttonStyle(.borderedProminent)
+                                                .padding()
+                                                
+                                                
+                                                Button("HIIT session 1"){
+                                                    data.HIITSession1.append(exercise)
+                                                }
+                                                .buttonStyle(.borderedProminent)
+                                                .padding()
+                                                
+                                                Button("HIIT session 2"){
+                                                    data.HIITSession2.append(exercise)
+                                                }
+                                                .buttonStyle(.borderedProminent)
+                                                .padding()
+                                                
+                                                Button("HIIT session 3"){
+                                                    data.HIITSession3.append(exercise)
+                                                }
+                                                .buttonStyle(.borderedProminent)
+                                                .padding()
+                                            }
+                                            
+                                        }
+                                    }
+                                }
+                            } label: {
+                                HStack{
+                                    Image(exercise.display)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 50, height: 50, alignment: .center)
+                                        .padding(50)
+                                    VStack{
+                                        Text(exercise.name)
+                                        Text(exercise.bodypart)
+                                            .font(.footnote)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    .navigationTitle("Exercise Glossary")
+                    .searchable(text: $searchText)
+                    
+                }
+            }
+        }
+    }
+struct ExerciseGlossaryView_Previews: PreviewProvider {
+        static var previews: some View {
+            ExerciseGlossaryView()
+                .environmentObject(ExerciseGlossary())
+        }
+    }
