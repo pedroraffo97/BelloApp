812d6b49d5be657460c45e45ddd95fa822dde0f6
AlonsoRaffo <59334649+pedroraffo97@users.noreply.github.com>
2025-02-28 05:10:57 -0500

    UserInformationView.swift.version5

--- UserInformationView.swift
+++ UserInformationView.swift
@@ -10,46 +10,51 @@ import Foundation
 import SwiftUI
 
 struct UserInformationView: View {
-    @State var new_user = User(name: "", age: 0, sex: "", weight: 0, height: 0, amr: 0)
-    @State var StringAge:String = ""
-    @State var StringWeight:String = ""
-    @State var StringHeight: String = ""
-    @State var StringAmr: String = ""
     @EnvironmentObject var userinformation: UserInformation
+    @State var StringAge: String = ""
+    @State var StringWeight: String = ""
+    @State var StringHeight: String = ""
+    
+    func UpdateUserInformation() {
+        if let firstUser = userinformation.UserInformationDataBase.first {
+            if let index = userinformation.UserInformationDataBase.firstIndex(where: {$0.id == firstUser.id}) {
+                userinformation.UserInformationDataBase[index].age = Double(StringAge) ?? 0
+                userinformation.UserInformationDataBase[index].weight = Double(StringWeight) ?? 0
+                userinformation.UserInformationDataBase[index].height = Double(StringHeight) ?? 0
+                userinformation.CalculateAMRwithUser(userID: firstUser.id)
+            }
+        }
+    }
+    
     var body: some View {
-        VStack{
-            List {
-                Section("Name"){
-                    TextField("Enter your name", text: $new_user.name)}
-                Section("Age") {
-                    TextField("Enter your age", text: $StringAge)}
-                Section("Sex") {
-                    TextField("Enter your sex", text: $new_user.sex)}
-                Section("Weight"){
-                    TextField ("Enter your weight", text: $StringWeight)}
-                Section("Height"){
-                    TextField("Enter your height", text: $StringHeight)}
-                Section("AMR") {
-                    Text("AMR: Activity Level Metabolic Rate")
-                    TextField("Enter your Activity Level Metabolic Rate", text: $StringAmr)}
-            }.toolbar{
-                ToolbarItem {
-                    Button("Enter"){
-                        if let DoubleAge = Double(StringAge),
-                           let DoubleWeight = Double(StringWeight),
-                           let DoubleHeight = Double (StringHeight),
-                           let DoubleAmr = Double(StringAmr){
-                            new_user.age = DoubleAge
-                            new_user.weight = DoubleWeight
-                            new_user.height = DoubleHeight
-                            new_user.amr = DoubleAmr
-                            userinformation.UserInformationDataBase.append(new_user)}
+        VStack {
+                List {
+                    ForEach( userinformation.UserInformationDataBase.indices, id: \.self){
+                        index in
+                        let information = userinformation.UserInformationDataBase[index]
+                        Section("Name"){
+                            TextField(information.name, text: $userinformation.UserInformationDataBase[index].name)}
+                        Section("Age") {
+                            TextField("\(String(information.age))", text: $StringAge)}
+                        Section("Sex") {
+                            TextField(information.sex, text: $userinformation.UserInformationDataBase[index].sex)}
+                        Section("Weight"){
+                            TextField ("\(String(information.weight))", text: $StringWeight)}
+                        Section("Height"){
+                            TextField("\(String(information.height))", text: $StringHeight)}
                     }
+                }
+                .toolbar{
+                    ToolbarItem {
+                        Button("Update"){
+                            UpdateUserInformation()
+                            
+                            }
+                        }
                 }
             }
         }
     }
-}
 struct UserInformationView_previews: PreviewProvider {
     static var previews: some View {
         UserInformationView()
