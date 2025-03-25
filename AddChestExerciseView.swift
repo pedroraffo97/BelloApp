e0ead63a50ed5e43fbfcda98e782668f684cd539
AlonsoRaffo <59334649+pedroraffo97@users.noreply.github.com>
2025-03-25 05:53:44 -0500

    AddChestExerciseView.swift.version2

--- AddChestExerciseView.swift
+++ AddChestExerciseView.swift
@@ -12,27 +12,51 @@ import SwiftUI
 struct AddChestExerciseView: View {
     @State var new_Exercise = Exercise(name: "",bodypart: "", information: "", execution:"", repetition:"")
     @EnvironmentObject var data: Routine
-    @Environment (\.dismiss) var dismiss
+    @Environment(\.dismiss) var dismiss
     var body: some View {
-        VStack(alignment:.leading) {
-            Form {
-                Section("Name"){
-                    TextField("Name", text: $new_Exercise.name)
+        NavigationView {
+            ScrollView{
+                VStack(alignment:.leading){
+                    Text("Name")
+                        .foregroundStyle(.white)
+                        .font(.headline)
+                    TextField("", text: $new_Exercise.name)
+                        .foregroundStyle(Color.white)
+                        .textFieldStyle(.plain)
+                        .frame(height: 30)
+                        .cornerRadius(8)
+                        .background(Color.black)
                 }
-                Section("Repetition"){
-                    TextField("Repetitions", text: $new_Exercise.repetition)
+                .padding()
+                VStack(alignment:.leading){
+                    Text("Repetition")
+                        .foregroundStyle(.white)
+                        .font(.headline)
+                    TextField("", text: $new_Exercise.repetition)
+                        .foregroundStyle(Color.white)
+                        .textFieldStyle(.plain)
+                        .frame(height: 30)
+                        .cornerRadius(8)
+                        .background(Color.black)
                 }
-                
+                .padding()
             }
-        }.toolbar {
-            ToolbarItem {
-                Button("Add"){
-                    data.chest_exercises.append(new_Exercise)
-                    dismiss()
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        data.chest_exercises.append(new_Exercise)
+                        dismiss()
+                    } label: {
+                        Image(systemName: "plus")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                            .foregroundStyle(Color.white)
+                        }
+                    }
                 }
+            .background(Color.black.opacity(0.9))
+            .navigationTitle("Add Exercise")
             }
-        }
-        
         }
     }
 
@@ -42,3 +66,4 @@ struct AddChestExerciseView_previews: PreviewProvider {
             .environmentObject(Routine())
     }
 }
+
